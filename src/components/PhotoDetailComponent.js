05c4dcec5a707452624f152725c3615b8d554954
brotherhui521 <brotherhui521@gmail.com>
2020-06-15 14:49:25 -0700

    full page view Photo using react-images

--- src/components/PhotoDetailComponent.js
+++ src/components/PhotoDetailComponent.js
@@ -7,33 +7,56 @@ import {
   CardSubtitle,
   Button,
   CardTitle,
-  BreadcrumbItem
+  BreadcrumbItem,
+  
+  
+  
 } from "reactstrap";
+import Carousel, {Modal, ModalGateway} from 'react-images';
+import Image from 'react-bootstrap/Image'
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function RenderPhoto({ photo }) {
+function RenderPhoto(props) {
+  
   return (
     <div className="container">
-    <div className="row">
-    <BreadcrumbItem style={{ marginLeft: 14 }}>
-            <Link to="/photos">Photos</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>Photo Detail</BreadcrumbItem>
-    </div>
-    <div className="row" >
-      <div className="col col-12 col-md-6 col-lg-5">
-        <Card style={{ marginTop: "10px" ,marginBottom:"10px" }}>
-          <CardImg top width="100%" src={photo.image} alt="Photo here" />
-          <CardBody>
-            <CardTitle>{photo.name}</CardTitle>
-            <CardSubtitle>Location needed</CardSubtitle>
-            <CardText>add photo. description</CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
+      <div className="row">
+        <BreadcrumbItem style={{ marginLeft: 14 }}>
+          <Link to="/photos">Photos</Link>
+        </BreadcrumbItem>
+        <BreadcrumbItem active>Photo Detail</BreadcrumbItem>
       </div>
-    </div>
+      <div className="row">
+        <div className="col col-12 col-md-6 col-lg-5">
+          <Card style={{ marginTop: "10px", marginBottom: "10px" }}>
+            <CardImg
+              top
+              width="100%"
+              src={props.photo.image}
+              alt="Photo here"
+              onClick={props.toggleModal}
+            />
+            <CardBody>
+              <CardTitle>{props.photo.name}</CardTitle>
+              <CardSubtitle>Location needed</CardSubtitle>
+              <CardText>add photo. description</CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+        <ModalGateway>
+          {props.isModalOpen?(
+            <Modal onClose={props.toggleModal}>
+            <Carousel views={props.image}></Carousel>
+
+            
+          </Modal>
+          ):null}
+        </ModalGateway>
+          
+       
     </div>
   );
 }
@@ -41,10 +64,33 @@ function RenderPhoto({ photo }) {
 class PhotoDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isModalOpen: false,
+      image:[{src:props.photo.image}]
+    };
+   
+    this.toggleModal=this.toggleModal.bind(this);
   }
+
+  toggleModal(){
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  };
+  
   render() {
-    return <RenderPhoto photo={this.props.photo}></RenderPhoto>;
+    
+    return (
+      <div>
+        <RenderPhoto
+          photo={this.props.photo}
+          toggleModal={this.toggleModal}
+          isModalOpen={this.state.isModalOpen}
+          image={this.state.image}
+
+        ></RenderPhoto>
+      </div>
+    );
   }
 }
 
